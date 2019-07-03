fa4a33fcdd7d7ff98f341b50bf195556aa998b4e
AndyTechGuy <andytechdude@gmail.com>
2019-07-03 02:02:28 -0230

    Add need system with a food need, satisfied by visiting marketplaces.

---
+++ src/main/java/org/terasology/metalrenegades/ai/component/NeedsComponent.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.metalrenegades.ai.component;
+
+import org.terasology.entitySystem.Component;
+
+/**
+ * Component which keeps track of a citizens current need status.
+ */
+public class NeedsComponent implements Component {
+
+    /**
+     * The maximum food value that this character can have.
+     */
+    public float maxFoodCapacity = 20;
+
+    /**
+     * The rate of reduction of food value, in food unit per second.
+     */
+    public float foodReductionRate = 1f;
+
+    /**
+     * The food minimum, when foodValue slips below this value a particular behavior is initiated.
+     */
+    public float foodGoal = 5;
+
+    /**
+     * The current food value of this citizen.
+     */
+    public float foodValue = 10;
+
+}
---
+++ src/main/java/org/terasology/metalrenegades/ai/system/NeedsSystem.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.metalrenegades.ai.system;
+
+import org.terasology.entitySystem.entity.EntityManager;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
+import org.terasology.metalrenegades.ai.component.NeedsComponent;
+import org.terasology.registry.In;
+
+/**
+ * Manages needs for all citizens with {@link NeedsComponent}.
+ */
+@RegisterSystem(value = RegisterMode.AUTHORITY)
+public class NeedsSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
+
+    /**
+     * Time in seconds required between need checks.
+     */
+    private static final int NEEDS_CHECK_DELAY = 60;
+
+    /**
+     * The time since last need check.
+     */
+    private float checkTimer;
+
+    @In
+    private EntityManager entityManager;
+
+    @Override
+    public void update(float delta) {
+        checkTimer += delta;
+
+        if (checkTimer > NEEDS_CHECK_DELAY) {
+            for (EntityRef entity : entityManager.getEntitiesWith(NeedsComponent.class)) {
+                NeedsComponent needsComponent = entity.getComponent(NeedsComponent.class);
+
+                needsComponent.foodValue -= needsComponent.foodReductionRate;
+                entity.saveComponent(needsComponent);
+            }
+
+            checkTimer = 0;
+        }
+    }
+}
