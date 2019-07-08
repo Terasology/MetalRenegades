790a63728ba96ca73014bcb0b7455b53bc54a00c
Mayant Mukul <mayantmukul@hotmail.com>
2019-07-09 03:09:17 +0530

    Modifications to the starting inventory

--- src/main/java/org/terasology/metalrenegades/StartingInventorySystem.java
+++ src/main/java/org/terasology/metalrenegades/StartingInventorySystem.java
@@ -18,19 +18,16 @@ package org.terasology.metalrenegades;
 import org.terasology.combatSystem.inventory.CombatStartingInventory;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.EventPriority;
-import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.logic.inventory.InventoryManager;
-import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
 import org.terasology.registry.In;
 import org.terasology.world.block.BlockManager;
 import org.terasology.world.block.items.BlockItemFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class StartingInventorySystem extends BaseComponentSystem {
@@ -52,16 +49,20 @@ public class StartingInventorySystem extends BaseComponentSystem {
         int numBullets = 32;
 
         BlockItemFactory blockItemFactory = new BlockItemFactory(entityManager);
-        Map<EntityRef, Integer> items = new HashMap<>();
+        Set<EntityRef> items = new HashSet<>();
 
-        items.put(entityManager.create("core:pickaxe"), 1);
-        items.put(entityManager.create("core:shovel"), 1);
-        items.put(entityManager.create("core:axe"), 1);
-        items.put(blockItemFactory.newInstance(blockManager.getBlockFamily("core:Torch"), numTorches), 1);
-        items.put(entityManager.create("core:explodeTool"), 1);
-        items.put(entityManager.create("core:railgunTool"), 1);
-        items.put(entityManager.create("MetalRenegades:pistol"), 1);
-        items.put(entityManager.create("MetalRenegades:bulletItem"), numBullets);
+        items.add(entityManager.create("MetalRenegades:pistol"));
+
+        for (int i = 0; i < numBullets; i++) {
+            items.add(entityManager.create("MetalRenegades:bulletItem"));
+        }
+
+        items.add(entityManager.create("core:pickaxe"));
+        items.add(entityManager.create("core:shovel"));
+        items.add(entityManager.create("core:axe"));
+        items.add(blockItemFactory.newInstance(blockManager.getBlockFamily("core:Torch"), numTorches));
+        items.add(entityManager.create("core:explodeTool"));
+        items.add(entityManager.create("core:railgunTool"));
 
         combatStartingInventory.setItems(items);
     }
