1b7e25107b4179fb9cb1ad0e207731e3a3aad5c4
Mayant Mukul <mayantmukul@hotmail.com>
2019-08-08 14:16:23 +0530

    Destroy beacon on quest complete

--- src/main/java/org/terasology/metalrenegades/quests/FetchQuestSystem.java
+++ src/main/java/org/terasology/metalrenegades/quests/FetchQuestSystem.java
@@ -27,32 +27,56 @@ import org.terasology.entitySystem.prefab.Prefab;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.inventory.InventoryManager;
 import org.terasology.logic.location.LocationComponent;
 import org.terasology.logic.nameTags.NameTagComponent;
 import org.terasology.math.geom.Rect2i;
 import org.terasology.math.geom.Vector3f;
+import org.terasology.metalrenegades.economy.systems.CurrencyManagementSystem;
+import org.terasology.network.ClientComponent;
+import org.terasology.protobuf.EntityData;
 import org.terasology.registry.In;
 import org.terasology.rendering.nui.Color;
+import org.terasology.tasks.CollectBlocksTask;
+import org.terasology.tasks.Task;
+import org.terasology.tasks.components.QuestComponent;
 import org.terasology.tasks.components.QuestListComponent;
 import org.terasology.tasks.components.QuestSourceComponent;
 import org.terasology.tasks.events.BeforeQuestEvent;
+import org.terasology.tasks.events.QuestCompleteEvent;
 import org.terasology.tasks.events.StartTaskEvent;
 import org.terasology.utilities.Assets;
+import sun.text.resources.et.FormatData_et;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-@RegisterSystem(RegisterMode.CLIENT)
+@RegisterSystem(RegisterMode.AUTHORITY)
 public class FetchQuestSystem extends BaseComponentSystem {
 
     @In
     private EntityManager entityManager;
 
+    @In
+    private CurrencyManagementSystem currencyManagementSystem;
+
+    @In
+    private InventoryManager inventoryManager;
+
     private EntityRef activeQuestEntity;
+    private List<FetchedItem> fetchedItems = new ArrayList<>();
 
     private final String HOME_TASK_ID = "returnHome";
     private final String FETCH_QUEST_ID = "FetchQuest";
+    private final int REWARD = 50;
+
+    @Override
+    public void postBegin() {
+        activeQuestEntity = EntityRef.NULL;
+    }
 
     @ReceiveEvent(components = GenericBuildingComponent.class)
     public void onMarketPlaceSpawn(BuildingEntitySpawnedEvent event, EntityRef entityRef) {
@@ -95,6 +119,15 @@ public class FetchQuestSystem extends BaseComponentSystem {
     @ReceiveEvent
     public void onQuestActivated(BeforeQuestEvent event, EntityRef questItem) {
         activeQuestEntity = questItem.getComponent(QuestSourceComponent.class).source;
+
+        QuestComponent questComponent = questItem.getComponent(QuestComponent.class);
+        List<Task> tasks = questComponent.tasks;
+        fetchedItems = tasks.stream()
+                .filter(task -> task instanceof CollectBlocksTask)
+                .map(task -> new FetchedItem(
+                        ((CollectBlocksTask) task).getItemId(),
+                        ((CollectBlocksTask) task).getTargetAmount()
+                )).collect(Collectors.toList());
     }
 
     @ReceiveEvent
@@ -109,6 +142,53 @@ public class FetchQuestSystem extends BaseComponentSystem {
         if (beaconOptional.isPresent()) {
             EntityRef beacon = entityManager.create(beaconOptional.get(), locationComponent.getWorldPosition());
             activeQuestEntity.destroy();
+            activeQuestEntity = beacon;
+        }
+    }
+
+    @ReceiveEvent
+    public void onQuestComplete(QuestCompleteEvent event, EntityRef client) {
+        if (event.isSuccess()) {
+            // Remove items from inventory
+//            removeItems(client);
+
+            // Destroy the beacon
+            activeQuestEntity.destroy();
+
+            // Pay the player
+            currencyManagementSystem.changeWallet(REWARD);
+        }
+    }
+
+    private void removeItems(EntityRef client) {
+//        ClientComponent component = client.getComponent(ClientComponent.class);
+//        EntityRef character = component.character;
+//        EntityRef item = EntityRef.NULL;
+//
+//        try {
+//            for (int i = 0; i < inventoryManager.getNumSlots(character); i++) {
+//                EntityRef current = inventoryManager.getItemInSlot(character, i);
+//                if (!EntityRef.NULL.equals(current)) {
+//                    item = current;
+//                    break;
+//                }
+//            }
+//            inventoryManager.removeItem(localPlayer.getCharacterEntity(), EntityRef.NULL, item, true, 1);
+//        } catch (Exception e) {
+////            logger.error("Could not create entity from {}. Exception: {}", name, e.getMessage());
+//        }
+    }
+
+    class FetchedItem {
+        public String itemId;
+        public int amount;
+
+        public FetchedItem(String itemId, int amount) {
+            this.itemId = itemId;
+            this.amount = amount;
+        }
+
+        public FetchedItem() {
         }
     }
 }
