bddef0f60e0364d17a3f5a85f6ec5fd03c72cf74
agent-q1 <suhassuhas2001@gmail.com>
2020-07-13 16:30:11 +0530

    feat: Added changes requested

--- src/main/java/org/terasology/metalrenegades/ai/system/CitizenDeathSystem.java
+++ src/main/java/org/terasology/metalrenegades/ai/system/CitizenDeathSystem.java
@@ -5,25 +5,16 @@ package org.terasology.metalrenegades.ai.system;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.EventPriority;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.characters.AliveCharacterComponent;
-import org.terasology.logic.characters.CharacterComponent;
-import org.terasology.logic.characters.CharacterTeleportEvent;
 import org.terasology.logic.health.BeforeDestroyEvent;
-import org.terasology.logic.health.DestroyEvent;
-import org.terasology.logic.health.HealthComponent;
-import org.terasology.logic.health.event.RestoreFullHealthEvent;
-import org.terasology.logic.players.PlayerCharacterComponent;
+import org.terasology.metalrenegades.ai.component.CitizenComponent;
 import org.terasology.metalrenegades.ai.event.CitizenDeathEvent;
 import org.terasology.metalrenegades.minimap.events.RemoveCharacterFromOverlayEvent;
-import org.terasology.registry.In;
-import org.terasology.world.time.WorldTimeEvent;
 
 /**
  * System to handle Citizen Deaths within Metal Renegades
@@ -36,14 +27,18 @@ public class CitizenDeathSystem extends BaseComponentSystem {
 
 
     @ReceiveEvent(priority = EventPriority.PRIORITY_HIGH)
-    public void onCitizenDeath(BeforeDestroyEvent event, EntityRef entityRef,
-                               AliveCharacterComponent aliveCharacterComponent) {
-        entityRef.send(new RemoveCharacterFromOverlayEvent());
+    public void onEntityDestroyed(BeforeDestroyEvent event, EntityRef entityRef,
+                                  CitizenComponent citizenComponent) {
+        entityRef.send(new CitizenDeathEvent());
+        event.consume();
+    }
 
+    @ReceiveEvent
+    public void onCitizenDeath(CitizenDeathEvent event, EntityRef entityRef) {
+        entityRef.send(new RemoveCharacterFromOverlayEvent());
         logger.info("{} has died ", entityRef.toString());
         entityRef.destroy();
 
-        event.consume();
     }
 
 }
