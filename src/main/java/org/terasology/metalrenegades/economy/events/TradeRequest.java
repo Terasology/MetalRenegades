7dab4de66bec4090b46fa246e15caacbcae016d4
AndyTechGuy <andytechdude@gmail.com>
2020-07-24 01:26:10 -0230

    Add small-form license headings to multiplayer trade system upgrades

--- src/main/java/org/terasology/metalrenegades/economy/events/TradeRequest.java
+++ src/main/java/org/terasology/metalrenegades/economy/events/TradeRequest.java
@@ -1,3 +1,5 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.metalrenegades.economy.events;
 
 import org.terasology.entitySystem.entity.EntityRef;
