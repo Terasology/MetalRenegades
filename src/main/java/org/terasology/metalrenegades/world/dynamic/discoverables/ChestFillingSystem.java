4a17b141a9ea25b044bf8bfc8cdd0f4ad31b1058
AndyTechGuy <andytechdude@gmail.com>
2020-08-08 01:25:47 -0230

    Upgrade hidden chest filling system to use prefabs

--- src/main/java/org/terasology/metalrenegades/world/dynamic/discoverables/ChestFillingSystem.java
+++ src/main/java/org/terasology/metalrenegades/world/dynamic/discoverables/ChestFillingSystem.java
@@ -2,8 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.metalrenegades.world.dynamic.discoverables;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.prefab.PrefabManager;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
@@ -11,9 +14,15 @@ import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
 import org.terasology.logic.inventory.InventoryComponent;
 import org.terasology.logic.inventory.InventoryManager;
 import org.terasology.logic.inventory.ItemComponent;
+import org.terasology.metalrenegades.economy.systems.MarketCitizenSpawnSystem;
 import org.terasology.registry.In;
 import org.terasology.utilities.random.FastRandom;
 import org.terasology.utilities.random.Random;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Checks for any chests that have a {@link DiscoverableChestComponent} attached and fills it with items.
@@ -21,45 +30,22 @@ import org.terasology.utilities.random.Random;
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class ChestFillingSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
 
-    /**
-     * Items that generate in chests as stacks of five.
-     */
-    private final String[] bulkItems = {
-            "AdditionalFruits:Blueberry",
-            "AdditionalFruits:Raspberry",
-            "AdditionalFruits:Strawberry",
-            "AdditionalVegetables:Potato",
-            "MetalRenegades:bulletItem"
-    };
-
-    /**
-     * Items that generate in chests as stacks of one.
-     */
-    private final String[] singleItems = {
-            "CoreAssets:pickaxe",
-            "CoreAssets:axe",
-            "CoreAssets:shovel",
-            "MetalRenegades:emptyCup",
-            "MetalRenegades:pistol",
-            "MetalRenegades:emerald",
-            "MetalRenegades:ruby",
-            "MetalRenegades:sapphire"
-    };
+    private Logger logger = LoggerFactory.getLogger(ChestFillingSystem.class);
 
     /**
      * The chance out of one that any particular chest slot will contain an item.
      */
     private static final float ITEM_CHANCE = 0.08f;
 
-    /**
-     * The chance out of one that a chest slot with an item in it will be a bulk item vs. a single item.
-     */
-    private static final float BULK_SINGLE_RATIO = 0.75f;
+    private Map<String, Integer> hiddenItemsRegistry = new HashMap<>();
 
     /**
      * The random number provider that determines the items generated inside a discoverable chest.
      */
     private Random random;
+
+    @In
+    private PrefabManager prefabManager;
 
     @In
     private EntityManager entityManager;
@@ -70,6 +56,17 @@ public class ChestFillingSystem extends BaseComponentSystem implements UpdateSub
     @Override
     public void initialise() {
         random = new FastRandom();
+
+        prefabManager.listPrefabs(DiscoverableItemConfigurationComponent.class).stream().forEach(prefab -> {
+            DiscoverableItemConfigurationComponent itemDef =
+                    prefab.getComponent(DiscoverableItemConfigurationComponent.class);
+
+            List<List<String>> configs = itemDef.items;
+            configs.stream().forEach(config -> {
+                Iterator<String> it = config.iterator();
+                hiddenItemsRegistry.put(it.next(), Integer.parseInt(it.next()));
+            });
+        });
     }
 
     @Override
@@ -112,15 +109,22 @@ public class ChestFillingSystem extends BaseComponentSystem implements UpdateSub
      * @return The chest slot item.
      */
     private EntityRef getSlotItem() {
-        EntityRef item;
-        if (random.nextFloat() < BULK_SINGLE_RATIO) {
-            item = entityManager.create(bulkItems[random.nextInt(bulkItems.length)]);
-            ItemComponent itemComponent = item.getComponent(ItemComponent.class);
-            itemComponent.stackCount = 5;
-            item.saveComponent(itemComponent);
-        } else {
-            item = entityManager.create(singleItems[random.nextInt(singleItems.length)]);
+        if (hiddenItemsRegistry.isEmpty()) {
+            initialise();
+
+            if (hiddenItemsRegistry.isEmpty()) { // still empty after initialization? something must be wrong
+                logger.warn("No items in the hidden chest registry!");
+            }
         }
+
+        Object[] itemURIs = hiddenItemsRegistry.keySet().toArray();
+        String randomURI = (String) itemURIs[random.nextInt(itemURIs.length)];
+
+        EntityRef item = entityManager.create(randomURI);
+        ItemComponent itemComponent = item.getComponent(ItemComponent.class);
+        itemComponent.stackCount = hiddenItemsRegistry.get(randomURI).byteValue();
+        item.saveComponent(itemComponent);
+
         return item;
     }
 }
---
+++ src/main/java/org/terasology/metalrenegades/world/dynamic/discoverables/DiscoverableItemConfigurationComponent.java
@@ -0,0 +1,17 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.metalrenegades.world.dynamic.discoverables;
+
+import org.terasology.entitySystem.Component;
+
+import java.util.List;
+
+public class DiscoverableItemConfigurationComponent implements Component {
+
+    /**
+     * A list of all item definitions that can be generated in hidden chests.
+     */
+    public List<List<String>> items;
+
+}
