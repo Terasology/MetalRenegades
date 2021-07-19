96b0b81f0e5f0669874f823e6d87335ae417fba7
tolziplohu <sam@dirkback.com>
2021-07-19 16:13:47 -0500

    shrink transition zones between desert and scrubland

--- src/main/java/org/terasology/metalrenegades/world/dynamic/BaseBiomeProvider.java
+++ src/main/java/org/terasology/metalrenegades/world/dynamic/BaseBiomeProvider.java
@@ -14,7 +14,9 @@ import org.terasology.engine.world.generation.FacetProvider;
 import org.terasology.engine.world.generation.GeneratingRegion;
 import org.terasology.engine.world.generation.Produces;
 import org.terasology.engine.world.generation.Requires;
+import org.terasology.engine.world.generation.Updates;
 import org.terasology.engine.world.generation.facets.SurfaceHumidityFacet;
+import org.terasology.metalrenegades.world.rivers.RiverFacet;
 
 import java.util.Iterator;
 
@@ -52,7 +54,7 @@ public class BaseBiomeProvider implements FacetProvider {
         for (int i = 0; i < biomeData.length; i++) {
             Vector2ic pos = positions.next();
             // humidity goes from 0 to 0.3, so there's a 2/3 chance of desert
-            if (humidityData[i] > 0.2 + whiteNoise.noise(pos.x(), pos.y()) * 0.03) {
+            if (humidityData[i] > 0.2 + whiteNoise.noise(pos.x(), pos.y()) * 0.01) {
                 biomeData[i] = MRBiome.SCRUBLAND;
             } else {
                 biomeData[i] = CoreBiome.DESERT;
